5920c5e6318e6b614ce136f3f0f8d8f33951ba34
Sid Jagarlamudi <jaglings@gmail.com>
2020-04-13 16:52:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,17 +11,17 @@ def using_unshift(bouroughs_in_nyc, new_neighborhood)
 end
 
 def using_pop(continents)
- continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+ @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
      continents.pop
 end
 
 def pop_with_args(dog_breeds)
-    dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+    @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
     dog_breeds.pop(2)
 end
 
 def using_shift(my_favorite_cities)
-   my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+   @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
   my_favorite_cities.shift
 end
 
